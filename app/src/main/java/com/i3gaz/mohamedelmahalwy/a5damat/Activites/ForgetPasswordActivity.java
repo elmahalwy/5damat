6ea903b6fe86a7a4027ffc24333c0258c082e157
omarnagah <omarnagah3@gmail.com>
2018-09-27 16:10:31 +0200

    no.03

---
+++ app/src/main/java/com/i3gaz/mohamedelmahalwy/a5damat/Activites/ForgetPasswordActivity.java
@@ -0,0 +1,15 @@
+package com.i3gaz.mohamedelmahalwy.a5damat.Activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.i3gaz.mohamedelmahalwy.a5damat.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
