25c94a74df760acf3af5bf2c34e813fb41480cc5
omarnagah <omarnagah3@gmail.com>
2018-10-03 10:18:29 +0200

    no.019

--- app/src/main/java/com/I3gaz/mohamedelmahalwy/a5damat/Adapters/DevelopmentsAdapter.java
+++ app/src/main/java/com/I3gaz/mohamedelmahalwy/a5damat/Adapters/DevelopmentsAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -58,6 +59,8 @@ public class DevelopmentsAdapter extends RecyclerView.Adapter<DevelopmentsAdapte
         Spinner sp_time_for_development;
         @BindView(R.id.sp_time)
         Spinner sp_time;
+        @BindView(R.id.btn_add_more_developments)
+        Button btn_add_more_developments;
 
         public ViewHolder(View itemView) {
             super(itemView);
