2d140a63bf0ab96aa335db011e5b81def90866c9
omarnagah <omarnagah3@gmail.com>
2018-10-23 16:56:29 +0200

    no.051

--- app/src/main/java/com/I3gaz/mohamedelmahalwy/a5damat/Fragments/RealTimeMessageFragment.java
+++ app/src/main/java/com/I3gaz/mohamedelmahalwy/a5damat/Fragments/RealTimeMessageFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.I3gaz.mohamedelmahalwy.a5damat.Activites.HomeActivity;
 import com.I3gaz.mohamedelmahalwy.a5damat.Adapters.HomeAdapter;
@@ -24,6 +26,9 @@ import com.I3gaz.mohamedelmahalwy.a5damat.Network.RetroWeb;
 import com.I3gaz.mohamedelmahalwy.a5damat.Network.ServiceApi;
 import com.I3gaz.mohamedelmahalwy.a5damat.R;
 import com.I3gaz.mohamedelmahalwy.a5damat.Utils.SharedPrefManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -62,20 +67,37 @@ public class RealTimeMessageFragment extends Fragment {
 
     void initUi() {
         serviceDetails = (ServiceDetails) getArguments().getParcelable("response");
-        get_messages();
+//        get_messages();
+        Timer timer = new Timer ();
+        TimerTask hourlyTask = new TimerTask () {
+            @Override
+            public void run () {
+                // your code here...
+                get_messages();
+            }
+        };
+
+// schedule the task to run starting now and then every hour...
+        timer.schedule (hourlyTask, 0l, 1000*60);   // 1000*10*60 every 1 minut
     }
 
     void initEventDriven() {
+        iv_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_messages();
+            }
+        });
     }
 
     void get_messages() {
-        ((HomeActivity) getActivity()).showdialog();
+//        ((HomeActivity) getActivity()).showdialog();
         RetroWeb.getClient().create(ServiceApi.class).send_real_time_messages(String.valueOf(sharedPrefManager.getUserDate().getId()), String.valueOf(serviceDetails.getData().getOwnerId()), String.valueOf(serviceDetails.getData().getId()), "").enqueue(new Callback<RealTimeMessageModel>() {
             @Override
             public void onResponse(Call<RealTimeMessageModel> call, Response<RealTimeMessageModel> response) {
                 try {
                     Log.e("message_respone", response.toString());
-                    ((HomeActivity) getActivity()).dismis_dialog();
+//                    ((HomeActivity) getActivity()).dismis_dialog();
                     if (response.body().isValue()) {
                         realTimeMessageAdapter.addAll(response.body().getData().getMessages());
                         chat_recycler.setAdapter(realTimeMessageAdapter);
@@ -87,7 +109,7 @@ public class RealTimeMessageFragment extends Fragment {
 
             @Override
             public void onFailure(Call<RealTimeMessageModel> call, Throwable t) {
-                ((HomeActivity) getActivity()).dismis_dialog();
+//                ((HomeActivity) getActivity()).dismis_dialog();
                 handleException(getActivity(), t);
                 t.printStackTrace();
                 Log.e("t", String.valueOf(t));
@@ -98,29 +120,43 @@ public class RealTimeMessageFragment extends Fragment {
     }
 
     void send_messages() {
-        RetroWeb.getClient().create(ServiceApi.class).send_real_time_messages(String.valueOf(sharedPrefManager.getUserDate().getId()), String.valueOf(serviceDetails.getData().getOwnerId()), String.valueOf(serviceDetails.getData().getId()), et_chat.getText().toString()).enqueue(new Callback<RealTimeMessageModel>() {
-            @Override
-            public void onResponse(Call<RealTimeMessageModel> call, Response<RealTimeMessageModel> response) {
-                try {
-                    Log.e("message_respone", response.toString());
-                    if (response.body().isValue()) {
-                        et_chat.setText("");
+        boolean cancel = false;
+        View focusView = null;
+        if (TextUtils.isEmpty(et_chat.getText().toString())) {
+            cancel = true;
+            Toast.makeText(getContext(), "برجاء إضافه رساله لارسالها", Toast.LENGTH_SHORT).show();
+        }
+        if (cancel) {
 
+        } else {
+            RetroWeb.getClient().create(ServiceApi.class).send_real_time_messages(String.valueOf(sharedPrefManager.getUserDate().getId()),
+                    String.valueOf(serviceDetails.getData().getOwnerId()),
+                    String.valueOf(serviceDetails.getData().getId()),
+                    et_chat.getText().toString()).enqueue(new Callback<RealTimeMessageModel>() {
+                @Override
+                public void onResponse(Call<RealTimeMessageModel> call, Response<RealTimeMessageModel> response) {
+                    try {
+                        Log.e("message_response_send", response.toString());
+                        if (response.body().isValue()) {
+                            et_chat.setText("");
+                            get_messages();
+
+                        }
+                    } catch (Exception e) {
+                        Log.e("e", String.valueOf(e));
                     }
-                } catch (Exception e) {
-                    Log.e("e", String.valueOf(e));
                 }
-            }
 
-            @Override
-            public void onFailure(Call<RealTimeMessageModel> call, Throwable t) {
-                handleException(getActivity(), t);
-                t.printStackTrace();
-                Log.e("t", String.valueOf(t));
+                @Override
+                public void onFailure(Call<RealTimeMessageModel> call, Throwable t) {
+                    handleException(getActivity(), t);
+                    t.printStackTrace();
+                    Log.e("t", String.valueOf(t));
 
-            }
+                }
 
-        });
+            });
+        }
     }
 
 }
